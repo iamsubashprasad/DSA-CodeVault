bfcaf6966e420208a88f4d412974bcb5f1a2d1f4
subas <subas@LAPTOP-FQBPSMC9>
2023-05-02 08:09:40 +0530

    code updated

--- DSA/src/SDET/moveZeros.java
+++ DSA/src/SDET/moveZeros.java
@@ -8,16 +8,23 @@ public class moveZeros {
 		int i=0;
 		int arr[]= {0,1,0,3,12};
 		int temp=0;
+		int output[]= new int[arr.length];
 		
 		for (i=0;i<arr.length;i++) {
-			if(arr[i]!=0) {
-				arr[temp++]=arr[i];
-			}
-			
+			if(arr[i]>0) 
+				output[temp++]=arr[i];		
 		}
-       for(i=0;i<arr.length;i++) {
-    	   System.out.print(arr[i]+",");
-       }
+		for (i=0;i<output.length;i++) {
+			System.out.println(output[i]);
+		}
+		
 	}
-
 }
+		
+		
+		
+		
+		
+	
+      
+
