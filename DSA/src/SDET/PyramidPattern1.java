77df058f4bd367e4af4cd2a844181f1e25204f58
subas <subas@LAPTOP-FQBPSMC9>
2023-05-02 09:14:44 +0530

    pyramid_1

--- DSA/src/SDET/PyramidPattern1.java
+++ DSA/src/SDET/PyramidPattern1.java
@@ -5,7 +5,7 @@ public class PyramidPattern1 {
 	public static void main(String[] args) {
 		int n=5,i,j;
 		// TODO Auto-generated method stub
-		for (i=0; i<n; i++)
+		/*for (i=0; i<n; i++)
         {
             for ( j=n-i; j>1; j--)
             {
@@ -20,7 +20,20 @@ public class PyramidPattern1 {
             }
   
             System.out.println();
-        }
+        }*/
+		
+		for (i=0;i<n;i++) {
+			for (j=n-i;j>1;j--) {
+				System.out.print(" ");
+			}
+			for(j=0;j<=i;j++) {
+				System.out.print("* ");
+				
+			}
+			System.out.println();
+		
+		}
+		
     }
 
 	}
