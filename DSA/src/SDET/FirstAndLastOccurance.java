dd1665c13898264c34cd6bba81eade5316e2c226
subas <subas@LAPTOP-FQBPSMC9>
2023-05-03 08:16:07 +0530

    pattern

--- DSA/src/SDET/FirstAndLastOccurance.java
+++ DSA/src/SDET/FirstAndLastOccurance.java
@@ -4,21 +4,19 @@ public class FirstAndLastOccurance {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[]= {1,2,3,4,5,1};
+		int arr[]= {1,2,3,1,5};
 		int n= arr.length;
 		System.out.println();
 		int i,temp=0,k=1;
 		
-		for(i=0;i<=n-1;i++) {
-			if (arr[i]==k) {
-				arr[temp]=arr[i];
-				arr[i]=arr[i+1];
-				arr[i+1]=arr[temp];
+		for(i=0;i<n;i++) {
+			if (arr[i]!=k) {
+				arr[temp++]=arr[i];
 			}
 			
 		}
-		for(i=0;i<=arr.length-2;i++) {
-			System.out.println(arr[i]);
+		for(i=0;i<=arr.length-4;i++) {
+			System.out.print(arr[i]);
 		}
 			
 	}
