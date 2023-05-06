88cd3175b3de9c650bb6883554b63999cc871bfd
subas <subas@LAPTOP-FQBPSMC9>
2023-05-06 23:14:15 +0530

    sorting

---
+++ DSA/src/SDET/MergeTwoSortedArray.java
@@ -0,0 +1,31 @@
+package SDET;
+
+public class MergeTwoSortedArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Get the length of two arrays
+		 * declare third array with legth = arr1+arr2
+		 * i=j=k=0;
+		 * if a[i]<=a[j], a++,k++;
+		 * else k++,j++;
+		 * 
+		 */
+		
+		int arr1[]= {1,3,5,7,9};
+		int arr2[]= {2,4};
+		
+		int l1 = arr1.length;
+		int l2 = arr2.length;
+		
+		int mergesort[]= new int[l1+l2];
+		
+		
+		
+		
+		
+
+	}
+
+}
